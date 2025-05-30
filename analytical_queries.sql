1c00a30c09479598267663b3af21e35e4232668e
MMMakena <69667446+MMMakena@users.noreply.github.com>
2025-05-30 19:56:31 +0300

    analytical_queries.sql

--- analytical_queries.sql
+++ analytical_queries.sql
@@ -7,9 +7,18 @@ GROUP BY d.month
 ORDER BY d.month;
 
 -- Top Selling Products
-SELECT dp.name, SUM(fs.quantity_sold) AS total_units
-FROM fact_sales fs
-JOIN dim_product dp ON fs.product_id = dp.product_id
-GROUP BY dp.name
-ORDER BY total_units DESC
-LIMIT 5;
+SELECT 
+    dp.name AS product_name, 
+    SUM(fs.quantity_sold) AS total_units
+FROM 
+    fact_sales fs
+JOIN 
+    dim_product dp 
+ON 
+    fs.product_id = dp.product_id
+GROUP BY 
+    dp.name
+ORDER BY 
+    total_units DESC
+OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY;
+
